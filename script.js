198c14af15d72103293ad7950d7bff0711740978
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-18 19:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const btn = document.querySelector("#incrementBtn");
+const p = document.querySelector("#counter");
+function eventHandler(){
+	let val = p.innerText;
+	alert(val);
+	p.innerText = parseInt(val) + 1;
+}
+btn.addEventListener('click',eventHandler);
